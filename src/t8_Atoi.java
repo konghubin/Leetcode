a9fdb4e9f93c9ac48b0809cbfba07804a54feba0
kong <1400126414@qq.com>
2023-06-03 20:28:49 +0800

    t8_atoi

---
+++ src/t8_Atoi.java
@@ -0,0 +1,10 @@
+public class t8_Atoi {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public int myAtoi(String s){
+        
+        return 1;
+    }
+}
