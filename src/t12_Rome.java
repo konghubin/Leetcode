f45a2f00f39ad95b35f533cb6bb31ca66df94b33
kong <1400126414@qq.com>
2023-06-22 20:46:11 +0800

    T12_Rome

--- src/t12_Rome.java
+++ src/t12_Rome.java
@@ -1,4 +1,4 @@
-public class t12_Rome {
+public class T12_Rome {
     public String intToRoman(int num) {
         StringBuilder builder = new StringBuilder();
         int[] dataArray = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
@@ -14,7 +14,7 @@ public class t12_Rome {
     }
 
     public static void main(String[] args) throws Exception {
-        t12_Rome test = new t12_Rome();
+        T12_Rome test = new T12_Rome();
         System.out.println(test.intToRoman(1994)); 
     }
 }
